d519b18aea4cc2290e9b8b0b7a7c6fe7c5d468c9
zhuyiyi <zhuyiyi1990@outlook.com>
2022-02-24 17:31:23 +0800

    章节3:环境准备 课时24 Nacos服务-微服务服务注册

--- mall-coupon/src/main/java/com/msb/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/msb/mall/coupon/MallCouponApplication.java
@@ -3,9 +3,11 @@ package com.msb.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.msb.mall.coupon.dao")
+@EnableDiscoveryClient
 public class MallCouponApplication {
 
     public static void main(String[] args) {
--- mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
@@ -3,9 +3,11 @@ package com.msb.mall.order;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.msb.mall.order.dao")
+@EnableDiscoveryClient
 public class MallOrderApplication {
 
 	public static void main(String[] args) {
