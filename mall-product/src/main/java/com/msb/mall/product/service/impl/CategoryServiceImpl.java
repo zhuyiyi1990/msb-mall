ce1c8db828a558e1f460db4b238462e4c4d353a1
zhuyiyi <zhuyiyi1990@outlook.com>
2022-02-27 16:22:04 +0800

    章节4:业务开发-三级分类管理 课时34 分类后台查询(下)

--- mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/CategoryServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -33,11 +34,21 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
     @Override
     public List<CategoryEntity> queryPageWithTree(Map<String, Object> params) {
         List<CategoryEntity> categoryEntities = baseMapper.selectList(null);
-        List<CategoryEntity> list = categoryEntities.stream().filter(categoryEntity -> categoryEntity.getCatId() == 0).map(categoryEntity -> {
+        List<CategoryEntity> list = categoryEntities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0).map(categoryEntity -> {
+                    categoryEntity.setChildren(getCategoryChildren(categoryEntity, categoryEntities));
                     return categoryEntity;
                 }).sorted((entity1, entity2) -> (entity1.getSort() == null ? 0 : entity1.getSort()) - (entity2.getSort() == null ? 0 : entity2.getSort()))
                 .collect(Collectors.toList());
         return list;
+    }
+
+    private List<CategoryEntity> getCategoryChildren(CategoryEntity categoryEntity, List<CategoryEntity> categoryEntities) {
+        List<CategoryEntity> collect = categoryEntities.stream().filter(entity -> Objects.equals(entity.getParentCid(), categoryEntity.getCatId())).map((entity) -> {
+                    entity.setChildren(getCategoryChildren(entity, categoryEntities));
+                    return entity;
+                }).sorted((entity1, entity2) -> (entity1.getSort() == null ? 0 : entity1.getSort()) - (entity2.getSort() == null ? 0 : entity2.getSort()))
+                .collect(Collectors.toList());
+        return collect;
     }
 
 }
