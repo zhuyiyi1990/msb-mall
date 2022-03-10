801ada9e8e66748e233050771d97f81a65da6cbd
zhuyiyi <zhuyiyi1990@outlook.com>
2022-03-10 09:21:46 +0800

    章节5:业务开发-品牌管理 课时56 阿里云API代码上传图片

--- mall-product/src/test/java/com/msb/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/msb/mall/product/MallProductApplicationTests.java
@@ -1,13 +1,17 @@
 package com.msb.mall.product;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.msb.mall.product.entity.BrandEntity;
 import com.msb.mall.product.service.BrandService;
-import org.aspectj.weaver.ast.Var;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 @SpringBootTest
@@ -37,6 +41,20 @@ class MallProductApplicationTests {
         for (BrandEntity entity : list) {
             System.out.println(entity);
         }
+    }
+
+    @Test
+    public void testUploadFile() throws FileNotFoundException {
+        // Endpoint以华东1（杭州）为例，其它Region请按实际情况填写。
+        String endpoint = "yourEndpoint";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "yourAccessKeyId";
+        String accessKeySecret = "yourAccessKeySecret";
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        InputStream inputStream = new FileInputStream("D:\\localpath\\examplefile.txt");
+        ossClient.putObject("examplebucket", "exampledir/exampleobject.txt", inputStream);
+        ossClient.shutdown();
     }
 
 }
