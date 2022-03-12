2c30b5cb9b7b12e45a25ad4b1f26ee48686c6b35
朱一一 <zhuyiyi1990@outlook.com>
2022-03-12 16:01:29 +0800

    章节6:业务开发-属性管理 课时77 属性管理-属性组添加数据实现(上)

--- mall-product/src/main/java/com/msb/mall/product/entity/CategoryEntity.java
+++ mall-product/src/main/java/com/msb/mall/product/entity/CategoryEntity.java
@@ -8,6 +8,7 @@ import com.baomidou.mybatisplus.annotation.TableName;
 import java.io.Serializable;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
 /**
@@ -20,6 +21,7 @@ import lombok.Data;
 @Data
 @TableName("pms_category")
 public class CategoryEntity implements Serializable {
+
     private static final long serialVersionUID = 1L;
 
     /**
@@ -27,41 +29,50 @@ public class CategoryEntity implements Serializable {
      */
     @TableId
     private Long catId;
+
     /**
      * 分类名称
      */
     private String name;
+
     /**
      * 父分类id
      */
     private Long parentCid;
+
     /**
      * 层级
      */
     private Integer catLevel;
+
     /**
      * 是否显示[0-不显示，1显示]
      */
     @TableLogic(value = "1", delval = "0")
     private Integer showStatus;
+
     /**
      * 排序
      */
     private Integer sort;
+
     /**
      * 图标地址
      */
     private String icon;
+
     /**
      * 计量单位
      */
     private String productUnit;
+
     /**
      * 商品数量
      */
     private Integer productCount;
 
     @TableField(exist = false)
+    @JsonInclude(JsonInclude.Include.NON_EMPTY)
     private List<CategoryEntity> children;
 
 }
