bdadd5374c2267efe46bb7a64b5b3dad6bdfae4e
朱一一 <zhuyiyi1990@outlook.com>
2022-03-14 09:26:13 +0800

    章节7:业务开发-规格参数 课时86 参数规格-数据展示优化

--- mall-product/src/main/java/com/msb/mall/product/service/impl/AttrServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/AttrServiceImpl.java
@@ -1,13 +1,21 @@
 package com.msb.mall.product.service.impl;
 
+import com.msb.mall.product.dao.AttrAttrgroupRelationDao;
 import com.msb.mall.product.entity.AttrAttrgroupRelationEntity;
+import com.msb.mall.product.entity.AttrGroupEntity;
+import com.msb.mall.product.entity.CategoryEntity;
 import com.msb.mall.product.service.AttrAttrgroupRelationService;
+import com.msb.mall.product.service.AttrGroupService;
+import com.msb.mall.product.service.CategoryService;
+import com.msb.mall.product.vo.AttrResponseVo;
 import com.msb.mall.product.vo.AttrVO;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -26,6 +34,15 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
 
     @Autowired
     AttrAttrgroupRelationService attrAttrgroupRelationService;
+
+    @Autowired
+    AttrAttrgroupRelationDao attrAttrgroupRelationDao;
+
+    @Autowired
+    CategoryService categoryService;
+
+    @Autowired
+    AttrGroupService attrGroupService;
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
@@ -60,7 +77,26 @@ public class AttrServiceImpl extends ServiceImpl<AttrDao, AttrEntity> implements
             });
         }
         IPage<AttrEntity> page = this.page(new Query<AttrEntity>().getPage(params), wrapper);
-        return new PageUtils(page);
+        PageUtils pageUtils = new PageUtils(page);
+        List<AttrEntity> records = page.getRecords();
+        List<AttrResponseVo> list = records.stream().map((attrEntity) -> {
+            AttrResponseVo responseVo = new AttrResponseVo();
+            BeanUtils.copyProperties(attrEntity, responseVo);
+            CategoryEntity categoryEntity = categoryService.getById(attrEntity.getCatelogId());
+            if (categoryEntity != null) {
+                responseVo.setCatelogName(categoryEntity.getName());
+            }
+            AttrAttrgroupRelationEntity entity = new AttrAttrgroupRelationEntity();
+            entity.setAttrId(attrEntity.getAttrId());
+            AttrAttrgroupRelationEntity attrAttrgroupRelationEntity = attrAttrgroupRelationDao.selectOne(new QueryWrapper<AttrAttrgroupRelationEntity>().eq("attr_id", attrEntity.getAttrId()));
+            if (attrAttrgroupRelationEntity != null && attrAttrgroupRelationEntity.getAttrGroupId() != null) {
+                AttrGroupEntity attrGroupEntity = attrGroupService.getById(attrAttrgroupRelationEntity.getAttrGroupId());
+                responseVo.setGroupName(attrGroupEntity.getAttrGroupName());
+            }
+            return responseVo;
+        }).collect(Collectors.toList());
+        pageUtils.setList(list);
+        return pageUtils;
     }
 
 }
---
+++ mall-product/src/main/java/com/msb/mall/product/vo/AttrResponseVo.java
@@ -0,0 +1,12 @@
+package com.msb.mall.product.vo;
+
+import lombok.Data;
+
+@Data
+public class AttrResponseVo extends AttrVO {
+
+    private String catelogName;
+
+    private String groupName;
+
+}
