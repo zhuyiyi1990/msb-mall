816a2325c36a81f5f35f567450a659166b8e4ed2
朱一一 <zhuyiyi1990@outlook.com>
2022-03-18 16:59:19 +0800

    章节8:业务开发-商品服务 课时106 商品服务-存储商品基本信息和商品介绍信息

--- mall-product/src/main/java/com/msb/mall/product/entity/SpuInfoDescEntity.java
+++ mall-product/src/main/java/com/msb/mall/product/entity/SpuInfoDescEntity.java
@@ -1,15 +1,16 @@
 package com.msb.mall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
-import java.util.Date;
+
 import lombok.Data;
 
 /**
  * spu信息介绍
- * 
+ *
  * @author Yiyi Zhu
  * @email zhuyiyi1990@outlook.com
  * @date 2022-02-24 11:16:48
@@ -17,16 +18,18 @@ import lombok.Data;
 @Data
 @TableName("pms_spu_info_desc")
 public class SpuInfoDescEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
 
-	/**
-	 * 商品id
-	 */
-	@TableId
-	private Long spuId;
-	/**
-	 * 商品介绍
-	 */
-	private String decript;
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 商品id
+     */
+    @TableId(type = IdType.INPUT)
+    private Long spuId;
+
+    /**
+     * 商品介绍
+     */
+    private String decript;
 
 }
--- mall-product/src/main/java/com/msb/mall/product/service/impl/SpuInfoServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/SpuInfoServiceImpl.java
@@ -1,8 +1,14 @@
 package com.msb.mall.product.service.impl;
 
+import com.msb.mall.product.entity.SpuInfoDescEntity;
+import com.msb.mall.product.service.SpuInfoDescService;
 import com.msb.mall.product.vo.SpuInfoVO;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -19,6 +25,9 @@ import org.springframework.transaction.annotation.Transactional;
 @Service("spuInfoService")
 public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> implements SpuInfoService {
 
+    @Autowired
+    SpuInfoDescService spuInfoDescService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<SpuInfoEntity> page = this.page(new Query<SpuInfoEntity>().getPage(params), new QueryWrapper<SpuInfoEntity>());
@@ -28,7 +37,16 @@ public class SpuInfoServiceImpl extends ServiceImpl<SpuInfoDao, SpuInfoEntity> i
     @Override
     @Transactional
     public void saveSpuInfo(SpuInfoVO spuInfoVo) {
-
+        SpuInfoEntity spuInfoEntity = new SpuInfoEntity();
+        BeanUtils.copyProperties(spuInfoVo, spuInfoEntity);
+        spuInfoEntity.setCreateTime(new Date());
+        spuInfoEntity.setUpdateTime(new Date());
+        this.save(spuInfoEntity);
+        List<String> decripts = spuInfoVo.getDecript();
+        SpuInfoDescEntity descEntity = new SpuInfoDescEntity();
+        descEntity.setSpuId(spuInfoEntity.getId());
+        descEntity.setDecript(String.join(",", decripts));
+        spuInfoDescService.save(descEntity);
     }
 
 }
