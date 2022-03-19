be220819cb4574f36897eed39027165d9227de64
朱一一 <zhuyiyi1990@outlook.com>
2022-03-19 09:41:29 +0800

    章节8:业务开发-商品服务 课时113 商品服务-满减折扣会员价存储实现

--- mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SkuFullReductionServiceImpl.java
+++ mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SkuFullReductionServiceImpl.java
@@ -1,9 +1,18 @@
 package com.msb.mall.coupon.service.impl;
 
 import com.msb.common.dto.SkuReductionDTO;
+import com.msb.mall.coupon.entity.MemberPriceEntity;
+import com.msb.mall.coupon.entity.SkuLadderEntity;
+import com.msb.mall.coupon.service.MemberPriceService;
+import com.msb.mall.coupon.service.SkuLadderService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -18,6 +27,12 @@ import com.msb.mall.coupon.service.SkuFullReductionService;
 @Service("skuFullReductionService")
 public class SkuFullReductionServiceImpl extends ServiceImpl<SkuFullReductionDao, SkuFullReductionEntity> implements SkuFullReductionService {
 
+    @Autowired
+    private SkuLadderService ladderService;
+
+    @Autowired
+    private MemberPriceService memberPriceService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<SkuFullReductionEntity> page = this.page(new Query<SkuFullReductionEntity>().getPage(params), new QueryWrapper<SkuFullReductionEntity>());
@@ -26,7 +41,31 @@ public class SkuFullReductionServiceImpl extends ServiceImpl<SkuFullReductionDao
 
     @Override
     public void saveSkuReduction(SkuReductionDTO dto) {
-
+//        1.折扣
+        SkuLadderEntity ladderEntity = new SkuLadderEntity();
+        ladderEntity.setSkuId(dto.getSkuId());
+        ladderEntity.setFullCount(dto.getFullCount());
+        ladderEntity.setDiscount(dto.getDiscount());
+        ladderEntity.setAddOther(dto.getCountStatus());
+        if (ladderEntity.getFullCount() > 0) {
+            ladderService.save(ladderEntity);
+        }
+//        2.满减
+        SkuFullReductionEntity fullReductionEntity = new SkuFullReductionEntity();
+        BeanUtils.copyProperties(dto, fullReductionEntity);
+        if (fullReductionEntity.getFullPrice().compareTo(new BigDecimal(0)) == 1) {
+            this.save(fullReductionEntity);
+        }
+//        3.会员价
+        List<MemberPriceEntity> memberPriceEntities = dto.getMemberPrice().stream().map(item -> {
+            MemberPriceEntity priceEntity = new MemberPriceEntity();
+            priceEntity.setSkuId(dto.getSkuId());
+            priceEntity.setMemberLevelId(item.getId());
+            priceEntity.setMemberPrice(item.getPrice());
+            priceEntity.setAddOther(1);
+            return priceEntity;
+        }).collect(Collectors.toList());
+        memberPriceService.saveBatch(memberPriceEntities);
     }
 
 }
