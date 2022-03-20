c434f7e4386d3c0facd982ce06e10ec045f9c71a
朱一一 <zhuyiyi1990@outlook.com>
2022-03-20 09:47:41 +0800

    章节9:业务开发-库存管理 课时122 库存管理-仓库列表维护

--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareInfoServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/WareInfoServiceImpl.java
@@ -1,7 +1,9 @@
 package com.msb.mall.ware.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,18 +13,19 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.WareInfoDao;
 import com.msb.mall.ware.entity.WareInfoEntity;
 import com.msb.mall.ware.service.WareInfoService;
-
+import org.springframework.util.StringUtils;
 
 @Service("wareInfoService")
 public class WareInfoServiceImpl extends ServiceImpl<WareInfoDao, WareInfoEntity> implements WareInfoService {
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<WareInfoEntity> page = this.page(
-                new Query<WareInfoEntity>().getPage(params),
-                new QueryWrapper<WareInfoEntity>()
-        );
-
+        QueryWrapper<WareInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.eq("id", key).or().like("name", key).or().like("address", key).or().like("areacode", key);
+        }
+        IPage<WareInfoEntity> page = this.page(new Query<WareInfoEntity>().getPage(params), wrapper);
         return new PageUtils(page);
     }
 
