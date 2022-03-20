f164374a5ef4fde0e2a802fff9bfe147d33fc2f3
朱一一 <zhuyiyi1990@outlook.com>
2022-03-20 10:40:40 +0800

    章节9:业务开发-库存管理 课时124 库存管理-创建采购需求

--- mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseDetailServiceImpl.java
+++ mall-ware/src/main/java/com/msb/mall/ware/service/impl/PurchaseDetailServiceImpl.java
@@ -1,7 +1,9 @@
 package com.msb.mall.ware.service.impl;
 
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,18 +13,29 @@ import com.msb.common.utils.Query;
 import com.msb.mall.ware.dao.PurchaseDetailDao;
 import com.msb.mall.ware.entity.PurchaseDetailEntity;
 import com.msb.mall.ware.service.PurchaseDetailService;
-
+import org.springframework.util.StringUtils;
 
 @Service("purchaseDetailService")
 public class PurchaseDetailServiceImpl extends ServiceImpl<PurchaseDetailDao, PurchaseDetailEntity> implements PurchaseDetailService {
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<PurchaseDetailEntity> page = this.page(
-                new Query<PurchaseDetailEntity>().getPage(params),
-                new QueryWrapper<PurchaseDetailEntity>()
-        );
-
+        QueryWrapper<PurchaseDetailEntity> queryWrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            queryWrapper.and(w -> {
+                w.eq("purchase_id", key).or().eq("sku_id", key);
+            });
+        }
+        String status = (String) params.get("status");
+        if (!StringUtils.isEmpty(status)) {
+            queryWrapper.eq("status", status);
+        }
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            queryWrapper.eq("ware_id", wareId);
+        }
+        IPage<PurchaseDetailEntity> page = this.page(new Query<PurchaseDetailEntity>().getPage(params), queryWrapper);
         return new PageUtils(page);
     }
 
