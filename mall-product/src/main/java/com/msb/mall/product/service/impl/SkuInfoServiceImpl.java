75c99bd134a418480b620d63878bf24fc460a4a3
朱一一 <zhuyiyi1990@outlook.com>
2022-03-20 09:14:07 +0800

    章节8:业务开发-商品服务 课时121 商品服务-商品管理检索实现

--- mall-product/src/main/java/com/msb/mall/product/service/impl/SkuInfoServiceImpl.java
+++ mall-product/src/main/java/com/msb/mall/product/service/impl/SkuInfoServiceImpl.java
@@ -1,7 +1,10 @@
 package com.msb.mall.product.service.impl;
 
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -11,18 +14,50 @@ import com.msb.common.utils.Query;
 import com.msb.mall.product.dao.SkuInfoDao;
 import com.msb.mall.product.entity.SkuInfoEntity;
 import com.msb.mall.product.service.SkuInfoService;
-
+import org.springframework.util.StringUtils;
 
 @Service("skuInfoService")
 public class SkuInfoServiceImpl extends ServiceImpl<SkuInfoDao, SkuInfoEntity> implements SkuInfoService {
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
-        IPage<SkuInfoEntity> page = this.page(
-                new Query<SkuInfoEntity>().getPage(params),
-                new QueryWrapper<SkuInfoEntity>()
-        );
+        IPage<SkuInfoEntity> page = this.page(new Query<SkuInfoEntity>().getPage(params), new QueryWrapper<SkuInfoEntity>());
+        return new PageUtils(page);
+    }
 
+    @Override
+    public PageUtils queryPageByCondition(Map<String, Object> params) {
+        QueryWrapper<SkuInfoEntity> wrapper = new QueryWrapper<>();
+        String key = (String) params.get("key");
+        if (!StringUtils.isEmpty(key)) {
+            wrapper.and(w -> {
+                w.eq("sku_id", key).or().like("sku_name", key);
+            });
+        }
+        String catalogId = (String) params.get("catalogId");
+        if (!StringUtils.isEmpty(catalogId) && !"0".equalsIgnoreCase(catalogId)) {
+            wrapper.eq("catalog_id", catalogId);
+        }
+        String brandId = (String) params.get("brandId");
+        if (!StringUtils.isEmpty(brandId) && !"0".equalsIgnoreCase(brandId)) {
+            wrapper.eq("brand_id", brandId);
+        }
+        String min = (String) params.get("min");
+        if (!StringUtils.isEmpty(min)) {
+            wrapper.ge("price", min);
+        }
+        String max = (String) params.get("max");
+        if (!StringUtils.isEmpty(max)) {
+            try {
+                BigDecimal bigDecimal = new BigDecimal(max);
+                if (bigDecimal.compareTo(new BigDecimal(0)) == 1) {
+                    wrapper.le("price", max);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        IPage<SkuInfoEntity> page = this.page(new Query<SkuInfoEntity>().getPage(params), wrapper);
         return new PageUtils(page);
     }
 
