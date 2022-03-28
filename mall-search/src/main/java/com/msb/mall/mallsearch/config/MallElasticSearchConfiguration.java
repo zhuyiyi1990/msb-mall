a5795eac0b78e2fefab7f81aa0c3ef435c7336be
朱一一 <zhuyiyi1990@outlook.com>
2022-03-28 10:13:23 +0800

    章节10:全文检索引擎-ElasticSearch 课时162 全文检索引擎-ElasticSearch-应用-保存数据

--- mall-search/src/main/java/com/msb/mall/mallsearch/config/MallElasticSearchConfiguration.java
+++ mall-search/src/main/java/com/msb/mall/mallsearch/config/MallElasticSearchConfiguration.java
@@ -1,6 +1,7 @@
 package com.msb.mall.mallsearch.config;
 
 import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestClientBuilder;
 import org.elasticsearch.client.RestHighLevelClient;
@@ -9,6 +10,15 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class MallElasticSearchConfiguration {
+
+    public static final RequestOptions COMMON_OPTIONS;
+
+    static {
+        RequestOptions.Builder builder = RequestOptions.DEFAULT.toBuilder();
+//        builder.addHeader("Authorization", "Bearer " + TOKEN);
+//        builder.setHttpAsyncResponseConsumerFactory(new HttpAsyncResponseConsumerFactory.HeapBufferedResponseConsumerFactory(30 * 1024 * 1024 * 1024));
+        COMMON_OPTIONS = builder.build();
+    }
 
     @Bean
     public RestHighLevelClient restHighLevelClient() {
--- mall-search/src/test/java/com/msb/mall/mallsearch/MallSearchApplicationTests.java
+++ mall-search/src/test/java/com/msb/mall/mallsearch/MallSearchApplicationTests.java
@@ -1,6 +1,12 @@
 package com.msb.mall.mallsearch;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.msb.mall.mallsearch.config.MallElasticSearchConfiguration;
+import lombok.Data;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -14,6 +20,29 @@ class MallSearchApplicationTests {
     @Test
     void contextLoads() {
         System.out.println("--->" + client);
+    }
+
+    @Test
+    void saveIndex() throws Exception {
+        IndexRequest indexRequest = new IndexRequest("system");
+        indexRequest.id("1");
+//        indexRequest.source("name", "bobokaoya", "age", 18, "gender", "男");
+        User user = new User();
+        user.setName("bobo");
+        user.setAge(22);
+        user.setGender("男");
+        ObjectMapper objectMapper = new ObjectMapper();
+        String json = objectMapper.writeValueAsString(user);
+        indexRequest.source(json, XContentType.JSON);
+        IndexResponse index = client.index(indexRequest, MallElasticSearchConfiguration.COMMON_OPTIONS);
+        System.out.println(index);
+    }
+
+    @Data
+    class User {
+        private String name;
+        private Integer age;
+        private String gender;
     }
 
 }
