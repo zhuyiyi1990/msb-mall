8e2aa3df06017bcfd8755f1c756ae5b5329bb663
朱一一 <zhuyiyi1990@outlook.com>
2022-04-14 15:00:55 +0800

    章节14:缓存和分布式锁 课时251 SpringCache-自定义配置类后的属性文件生效问题

--- mall-product/src/main/java/com/msb/mall/product/config/MyCacheConfig.java
+++ mall-product/src/main/java/com/msb/mall/product/config/MyCacheConfig.java
@@ -1,6 +1,6 @@
 package com.msb.mall.product.config;
 
-import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.boot.autoconfigure.cache.CacheProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -12,10 +12,23 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 public class MyCacheConfig {
 
     @Bean
-    public RedisCacheConfiguration redisCacheConfiguration() {
+    public RedisCacheConfiguration redisCacheConfiguration(CacheProperties cacheProperties) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig();
         config = config.serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()));
         config = config.serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        CacheProperties.Redis redisProperties = cacheProperties.getRedis();
+        if (redisProperties.getTimeToLive() != null) {
+            config = config.entryTtl(redisProperties.getTimeToLive());
+        }
+        if (redisProperties.getKeyPrefix() != null) {
+            config = config.prefixCacheNameWith(redisProperties.getKeyPrefix());
+        }
+        if (!redisProperties.isCacheNullValues()) {
+            config = config.disableCachingNullValues();
+        }
+        if (!redisProperties.isUseKeyPrefix()) {
+            config = config.disableKeyPrefix();
+        }
         return config;
     }
 
