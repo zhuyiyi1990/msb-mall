29b2d8af47a0eeb350fe772c18acc5c1fc7ee87d
朱一一 <zhuyiyi1990@outlook.com>
2022-04-15 08:41:24 +0800

    章节15:检索服务 课时259 检索服务-检索条件VO封装

--- mall-search/src/main/java/com/msb/mall/mallsearch/controller/SearchController.java
+++ mall-search/src/main/java/com/msb/mall/mallsearch/controller/SearchController.java
@@ -1,5 +1,6 @@
 package com.msb.mall.mallsearch.controller;
 
+import com.msb.mall.mallsearch.vo.SearchParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -7,12 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class SearchController {
 
     @GetMapping("/list.html")
-    public String listPage(Long catalog3Id) {
-        return "index";
-    }
-
-    @GetMapping("/search.html")
-    public String searchPage(String keyword) {
+    public String listPage(SearchParam param) {
         return "index";
     }
 
