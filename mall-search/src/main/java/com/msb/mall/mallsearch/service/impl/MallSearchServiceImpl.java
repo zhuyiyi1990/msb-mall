ec84707337f19afc9a834b8d1c2364d93b305e82
朱一一 <zhuyiyi1990@outlook.com>
2022-04-15 15:25:29 +0800

    章节15:检索服务 课时268 检索服务-后台检索结构处理

--- mall-search/src/main/java/com/msb/mall/mallsearch/service/impl/MallSearchServiceImpl.java
+++ mall-search/src/main/java/com/msb/mall/mallsearch/service/impl/MallSearchServiceImpl.java
@@ -1,15 +1,42 @@
 package com.msb.mall.mallsearch.service.impl;
 
+import com.msb.mall.mallsearch.config.MallElasticSearchConfiguration;
 import com.msb.mall.mallsearch.service.MallSearchService;
 import com.msb.mall.mallsearch.vo.SearchParam;
 import com.msb.mall.mallsearch.vo.SearchResult;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MallSearchServiceImpl implements MallSearchService {
 
+    @Autowired
+    private RestHighLevelClient client;
+
     @Override
     public SearchResult search(SearchParam param) {
+        SearchResult result = null;
+        // 1. 准备检索的请求
+        SearchRequest request = buildSearchRequest(param);
+        try {
+            // 2.执行检索操作
+            SearchResponse response = client.search(request, MallElasticSearchConfiguration.COMMON_OPTIONS);
+            // 3.需要把检索的信息封装为SearchResult
+            result = buildSearchResult(response);
+        } catch (Exception e) {
+
+        }
+        return result;
+    }
+
+    private SearchRequest buildSearchRequest(SearchParam param) {
+        return null;
+    }
+
+    private SearchResult buildSearchResult(SearchResponse response) {
         return null;
     }
 
