acdabc92b4a90fb784cf93d8b45f9947d2265bae
朱一一 <zhuyiyi1990@outlook.com>
2022-04-28 15:15:35 +0800

    章节18:服务认证 课时314 认证服务-登录注册页面处理

---
+++ mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
@@ -0,0 +1,19 @@
+package com.msb.mall.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/login.html")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/reg.html")
+    public String reg() {
+        return "reg";
+    }
+
+}
