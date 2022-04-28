c5d452931679158a9665ef738b6fae797aae4631
朱一一 <zhuyiyi1990@outlook.com>
2022-04-28 15:57:38 +0800

    章节18:服务认证 课时315 认证服务-视图控制器

--- mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
+++ mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
@@ -1,12 +1,11 @@
 package com.msb.mall.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class LoginController {
 
-    @GetMapping("/login.html")
+    /*@GetMapping("/login.html")
     public String login() {
         return "login";
     }
@@ -14,6 +13,6 @@ public class LoginController {
     @GetMapping("/reg.html")
     public String reg() {
         return "reg";
-    }
+    }*/
 
 }
