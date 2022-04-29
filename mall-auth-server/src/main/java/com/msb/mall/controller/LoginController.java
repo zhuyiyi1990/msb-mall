9bda63b3686c3ce5a52b1f7a9a5baaa478defe31
朱一一 <zhuyiyi1990@outlook.com>
2022-04-29 14:59:45 +0800

    章节18:服务认证 课时320 认证服务-短信服务-功能实现

--- mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
+++ mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
@@ -1,9 +1,20 @@
 package com.msb.mall.controller;
 
+import com.msb.common.utils.R;
+import com.msb.mall.feign.ThirdPartyFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.UUID;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private ThirdPartyFeignService thirdPartyFeignService;
 
     /*@GetMapping("/login.html")
     public String login() {
@@ -14,5 +25,14 @@ public class LoginController {
     public String reg() {
         return "reg";
     }*/
+
+    @ResponseBody
+    @GetMapping("/sms/sendCode")
+    public R sendSmsCode(@RequestParam("phone") String phone) {
+        // 生成随机的验证码
+        String code = UUID.randomUUID().toString().substring(0, 5);
+        thirdPartyFeignService.sendSmsCode(phone, code);
+        return R.ok();
+    }
 
 }
