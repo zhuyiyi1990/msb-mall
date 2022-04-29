b5a8907e205e24c7dec93d19b9732ac699a48758
朱一一 <zhuyiyi1990@outlook.com>
2022-04-29 15:25:08 +0800

    章节18:服务认证 课时321 认证服务-短信服务-验证码存储

--- mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
+++ mall-auth-server/src/main/java/com/msb/mall/controller/LoginController.java
@@ -1,17 +1,23 @@
 package com.msb.mall.controller;
 
+import com.msb.common.constant.SMSConstant;
 import com.msb.common.utils.R;
 import com.msb.mall.feign.ThirdPartyFeignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @Autowired
     private ThirdPartyFeignService thirdPartyFeignService;
@@ -29,8 +35,9 @@ public class LoginController {
     @ResponseBody
     @GetMapping("/sms/sendCode")
     public R sendSmsCode(@RequestParam("phone") String phone) {
-        // 生成随机的验证码
+        // 生成随机的验证码 --》 把生成的验证码存储到Redis服务中 sms:code:15021042401 12345
         String code = UUID.randomUUID().toString().substring(0, 5);
+        redisTemplate.opsForValue().set(SMSConstant.SMS_CODE_PREFIX + phone, code, 10, TimeUnit.MINUTES);
         thirdPartyFeignService.sendSmsCode(phone, code);
         return R.ok();
     }
---
+++ mall-commons/src/main/java/com/msb/common/constant/SMSConstant.java
@@ -0,0 +1,10 @@
+package com.msb.common.constant;
+
+/**
+ * 短信 相关的常量
+ */
+public class SMSConstant {
+
+    public static final String SMS_CODE_PREFIX = "sms:code:";
+
+}
