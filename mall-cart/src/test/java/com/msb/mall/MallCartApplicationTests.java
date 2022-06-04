e68d229d6b17b0421c38b9266315ffbd48540737
朱一一 <zhuyiyi1990@outlook.com>
2022-06-04 21:25:18 +0800

    章节19:购物车 课时349 购物车-服务环境搭建

---
+++ mall-cart/src/test/java/com/msb/mall/MallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msb.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MallCartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
