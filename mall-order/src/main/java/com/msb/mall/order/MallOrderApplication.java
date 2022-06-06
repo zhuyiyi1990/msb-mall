98b0fafca19e48b5f86af57f234a1b0aa86d3710
朱一一 <zhuyiyi1990@outlook.com>
2022-06-06 14:57:19 +0800

    章节20:订单模块 课时365 订单模块-整合SpringSession

--- mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
+++ mall-order/src/main/java/com/msb/mall/order/MallOrderApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 /**
  * basePackages 制定Fegin接口的路径
  */
+@EnableRedisHttpSession
 @EnableFeignClients(basePackages = "com.msb.mall.order.feign")
 @EnableDiscoveryClient
 @SpringBootApplication
