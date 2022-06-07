0e004466e45d0326f59ea7e2fcb0bc5b8301722c
朱一一 <zhuyiyi1990@outlook.com>
2022-06-07 16:22:39 +0800

    章节20:订单模块 课时381 订单模块-提交订单-数据提交

--- mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
+++ mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
@@ -2,10 +2,12 @@ package com.msb.mall.order.web;
 
 import com.msb.mall.order.service.OrderService;
 import com.msb.mall.order.vo.OrderConfirmVo;
+import com.msb.mall.order.vo.OrderSubmitVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class OrderWebController {
@@ -19,6 +21,15 @@ public class OrderWebController {
         OrderConfirmVo confirmVo = orderService.confirmOrder();
         model.addAttribute("confirmVo", confirmVo);
         return "confirm";
+    }
+
+    @PostMapping("/orderSubmit")
+    public String orderSubmit(OrderSubmitVO vo) {
+        System.out.println("vo = " + vo);
+        // 验证是否重复提交
+        // 下订单
+        // 跳转到支付页面
+        return "";
     }
 
 }
