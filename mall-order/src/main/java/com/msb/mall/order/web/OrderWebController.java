1cac3d552b732e6093e2abafdbd90e93a02ccd76
朱一一 <zhuyiyi1990@outlook.com>
2022-06-18 08:35:47 +0800

    章节21:分布式事务 课时419 订单管理-订单列表

--- mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
+++ mall-order/src/main/java/com/msb/mall/order/web/OrderWebController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
@@ -54,6 +55,13 @@ public class OrderWebController {
             // 表示下单操作失败
             return "redirect:http://order.msb.com/toTrade";
         }
+    }
+
+    @PostMapping("/orderPay")
+    public String orderPay(@RequestParam("orderSn") String orderSn) {
+        // TODO 完成相关的支付操作
+        System.out.println("orderSn = " + orderSn);
+        return "list";
     }
 
 }
