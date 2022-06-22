382955fbdc71c88e683dff7e19c9bc6600e0e75c
朱一一 <zhuyiyi1990@outlook.com>
2022-06-22 10:29:52 +0800

    章节22:秒杀服务 课时427 秒杀服务-秒杀服务配置

--- mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
+++ mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SeckillSkuRelationServiceImpl.java
@@ -1,7 +1,10 @@
 package com.msb.mall.coupon.service.impl;
 
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import org.springframework.stereotype.Service;
+
 import java.util.Map;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -12,17 +15,20 @@ import com.msb.mall.coupon.dao.SeckillSkuRelationDao;
 import com.msb.mall.coupon.entity.SeckillSkuRelationEntity;
 import com.msb.mall.coupon.service.SeckillSkuRelationService;
 
-
 @Service("seckillSkuRelationService")
 public class SeckillSkuRelationServiceImpl extends ServiceImpl<SeckillSkuRelationDao, SeckillSkuRelationEntity> implements SeckillSkuRelationService {
 
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+        QueryWrapper<SeckillSkuRelationEntity> queryWrapper = new QueryWrapper<>();
+        String promotionSessionId = (String) params.get("promotionSessionId");
+        String key = (String) params.get("key");
+        queryWrapper.eq(StringUtils.isNotBlank(promotionSessionId), "promotion_session_id", promotionSessionId)
+                .eq(StringUtils.isNotBlank(key), "sku_id", key);
         IPage<SeckillSkuRelationEntity> page = this.page(
                 new Query<SeckillSkuRelationEntity>().getPage(params),
-                new QueryWrapper<SeckillSkuRelationEntity>()
+                queryWrapper
         );
-
         return new PageUtils(page);
     }
 
