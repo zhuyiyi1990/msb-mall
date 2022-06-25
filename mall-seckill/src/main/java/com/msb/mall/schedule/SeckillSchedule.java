bf7436c520992d74ad1fa576226bed8684a1ba2e
朱一一 <zhuyiyi1990@outlook.com>
2022-06-25 18:36:53 +0800

    章节22:秒杀服务 课时430 秒杀服务-异步定时任务实现

--- mall-seckill/src/main/java/com/msb/mall/schedule/SeckillSchedule.java
+++ mall-seckill/src/main/java/com/msb/mall/schedule/SeckillSchedule.java
@@ -1,6 +1,8 @@
 package com.msb.mall.schedule;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,12 +14,26 @@ import org.springframework.stereotype.Component;
  */
 @Slf4j
 @Component
+@EnableAsync
 @EnableScheduling
 public class SeckillSchedule {
 
+    /**
+     * 默认情况下定时任务是一个同步的任务，那我们需要他能够异步的去处理
+     * 1.我们可以把需要定时执行的任务交给异步处理器(如java.util.concurrent.CompletableFuture)来处理
+     * 2.我们还可以把需要执行的方法异步执行
+     * (1)@EnableAsync 开启异步任务的功能
+     * (2)@Async 标识希望异步执行的方法
+     */
+    @Async
     @Scheduled(cron = "* * * * * *")
     public void schedule() {
         log.info("定时任务.....");
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
 }
