fc8f475af11869e89b36175e01350bbfda10d256
朱一一 <zhuyiyi1990@outlook.com>
2022-06-26 18:16:22 +0800

    章节22:秒杀服务 课时433 秒杀服务-查询秒杀活动对应的SKU

--- mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SeckillSessionServiceImpl.java
+++ mall-coupon/src/main/java/com/msb/mall/coupon/service/impl/SeckillSessionServiceImpl.java
@@ -1,5 +1,8 @@
 package com.msb.mall.coupon.service.impl;
 
+import com.msb.mall.coupon.entity.SeckillSkuRelationEntity;
+import com.msb.mall.coupon.service.SeckillSkuRelationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
@@ -8,6 +11,7 @@ import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
@@ -22,6 +26,9 @@ import com.msb.mall.coupon.service.SeckillSessionService;
 @Service("seckillSessionService")
 public class SeckillSessionServiceImpl extends ServiceImpl<SeckillSessionDao, SeckillSessionEntity> implements SeckillSessionService {
 
+    @Autowired
+    SeckillSkuRelationService relationService;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<SeckillSessionEntity> page = this.page(
@@ -34,8 +41,16 @@ public class SeckillSessionServiceImpl extends ServiceImpl<SeckillSessionDao, Se
     @Override
     public List<SeckillSessionEntity> getLatest3DaysSession() {
         // 计算未来3天的时间
-        return this.list(new QueryWrapper<SeckillSessionEntity>().
+        List<SeckillSessionEntity> list = this.list(new QueryWrapper<SeckillSessionEntity>().
                 between("start_time", startTime(), endTime()));
+        List<SeckillSessionEntity> newList = list.stream().map(session -> {
+            // 根据对应的sessionId活动编号查询出对应的活动商品信息
+            List<SeckillSkuRelationEntity> relationEntities = relationService.list(new QueryWrapper<SeckillSkuRelationEntity>()
+                    .eq("promotion_session_id", session.getId()));
+            session.setRelationEntities(relationEntities);
+            return session;
+        }).collect(Collectors.toList());
+        return newList;
     }
 
     private String startTime() {
--- mall-seckill/src/main/java/com/msb/mall/service/impl/SeckillServiceImpl.java
+++ mall-seckill/src/main/java/com/msb/mall/service/impl/SeckillServiceImpl.java
@@ -3,8 +3,11 @@ package com.msb.mall.service.impl;
 import com.msb.common.utils.R;
 import com.msb.mall.feign.CouponFeignService;
 import com.msb.mall.service.SeckillService;
+import com.msb.mall.vo.SeckillSessionEntity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class SeckillServiceImpl implements SeckillService {
@@ -16,8 +19,11 @@ public class SeckillServiceImpl implements SeckillService {
     public void uploadSeckillSku3Days() {
         // 1. 通过OpenFeign 远程调用Coupon服务中接口来获取未来三天的秒杀活动的商品
         R r = couponFeignService.getLatest3DaysSession();
-        Object session = r.get("data");
-        // 2. 上架商品
+        if (r.getCode() == 0) {
+            // 表示查询操作成功
+            List<SeckillSessionEntity> session = (List<SeckillSessionEntity>) r.get("data");
+            // 2. 上架商品
+        }
     }
 
 }
